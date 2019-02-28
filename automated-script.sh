12e0b12fc533ccf13b56c0ebe49af92c79ee7239
Debasish Mishra <34345521+dishayan@users.noreply.github.com>
2019-03-01 01:27:44 +0800

    Update automated-script.sh

--- automated-script.sh
+++ automated-script.sh
@@ -3,4 +3,4 @@ git clone https://github.com/dishayan/Nexus-application-GCP-Kubernetes-.git
 cd /var/tmp/Nexus-application-GCP-Kubernetes-/
 docker build -t gcr.io/my-nexus-project-232423/nexus:v1 .
 gcloud container clusters create nexus-cluster --num-nodes=3 --zone=us-central1-a
-kubectl create -f cloud-nexus.yaml
+kubectl create -f gcloud-nexus.yaml
